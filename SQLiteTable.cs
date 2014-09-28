1dbe977622078264edb3698d8cfad3e2647211f7
Andrew Su <andrewsu01+github@gmail.com>
2014-09-28 18:02:02 -0400

    Initial Commit

---
+++ SQLiteTable.cs
@@ -0,0 +1,16 @@
+﻿namespace SQLiteHelper
+{
+    public class SQLiteTable
+    {
+        public string TableName = "";
+        public SQLiteColumnList Columns = new SQLiteColumnList();
+
+        public SQLiteTable()
+        { }
+
+        public SQLiteTable(string name)
+        {
+            TableName = name;
+        }
+    }
+}
